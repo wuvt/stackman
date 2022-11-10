6e41f92d4f0b1dbfaa8785813170189d799744d6
Duke Hill <baltibronx@gmail.com>
2022-11-10 18:59:20 -0500

    Added dropdown menu for albums, button now animated

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,6 +1,27 @@
 import styles from './Album.module.css';
 import React from "preact/compat";
-import {useState} from "preact/hooks";
+import {useCallback, useRef, useState} from "preact/hooks";
+import Track from "./Track";
+import useSpring from "../hooks/useSpring";
+import classnames from "../utils/classnames";
+
+const DropdownButton = (props: {dropdownOn: boolean, handleDropdown: Function}) => {
+    const dButtonRef = useRef<HTMLButtonElement>(null);
+
+    useSpring(props.dropdownOn, 0.6, 700, 28, s => {
+        if (dButtonRef.current) {
+            dButtonRef.current.style.transform = `rotate(${s.currentValue * -180}deg)`;
+        }
+    })
+
+    return (
+        <button id="dropdown" ref={dButtonRef} onClick={() => props.handleDropdown((prevState: any) => !prevState)} className={styles.dropdownBox}>
+            <svg viewBox="0 0 24 24">
+                <path fill="black" d="m12 15.375-6-6 1.4-1.4 4.6 4.6 4.6-4.6 1.4 1.4Z"/>
+            </svg>
+        </button>
+    );
+}
 
 const Album = (props: { album: any, cUUID: string, handleInfo: Function; }) => {
     const stackColors: {[key: string]: string} = {
@@ -10,12 +31,9 @@ const Album = (props: { album: any, cUUID: string, handleInfo: Function; }) => {
 
 
     const [ dropdownOn, setDropdownOn ] = useState(false);
-    function handleDropdown() {
-        setDropdownOn(!dropdownOn);
-        console.log("cUUID: ",props.cUUID);
-    }
 
     return (
+        <div>
         <div class={styles.albumContainer}>
             <div class={styles.stackBox} style={{backgroundColor: (stackColors[props.album.collection] || 'gray')}}>
             </div>
@@ -38,16 +56,20 @@ const Album = (props: { album: any, cUUID: string, handleInfo: Function; }) => {
                 {props.album.year}
             </div>
             <button id="info" onClick={() => props.handleInfo(props.album.uuid)} class={props.album.uuid===props.cUUID ? styles.infoBoxOn: styles.infoBoxOff}>
-                <svg className="w-6 h-6" viewBox="0 0 24 24">
+                <svg class={styles.infoSVG} viewBox="0 0 24 24">
                     <path fill={props.album.uuid===props.cUUID  ? "white": "black"}
                           d="M11 17h2v-6h-2Zm1-8q.425 0 .713-.288Q13 8.425 13 8t-.287-.713Q12.425 7 12 7t-.712.287Q11 7.575 11 8t.288.712Q11.575 9 12 9Zm0 13q-2.075 0-3.9-.788-1.825-.787-3.175-2.137-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175 1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138 1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175-1.35 1.35-3.175 2.137Q14.075 22 12 22Zm0-2q3.35 0 5.675-2.325Q20 15.35 20 12q0-3.35-2.325-5.675Q15.35 4 12 4 8.65 4 6.325 6.325 4 8.65 4 12q0 3.35 2.325 5.675Q8.65 20 12 20Zm0-8Z"/>
                 </svg>
             </button>
-            <button id="dropdown" onClick={handleDropdown} class={styles.dropdownBox}>
-                <svg viewBox="0 0 24 24">
-                    <path fill="black" d="m12 15.375-6-6 1.4-1.4 4.6 4.6 4.6-4.6 1.4 1.4Z"/>
-                </svg>
-            </button>
+            <DropdownButton dropdownOn={dropdownOn} handleDropdown={setDropdownOn}></DropdownButton>
+        </div>
+        {dropdownOn && <div class={styles.tracksBox}>
+            {props.album.tracks.map((track: any, index: number) => {
+                    return <Track album={props.album} track={track} index={index}></Track>
+                }
+            )}
+        </div>
+        }
         </div>
     );
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -29,7 +29,7 @@ const FilterBar = (props: { newOn: boolean, handleNewButton: Function;}) => {
       </input>
       <Select name="stacks" label="Current stack" items={stacks}></Select>
         <button id="newButton" class={props.newOn ? styles.newButtonOn: styles.newButtonOff} onClick={() => props.handleNewButton()}>
-            <svg className="w-6 h-6" viewBox="0 0 20 20">
+            <svg class={styles.newSVG} viewBox="0 0 20 20">
                 <path fill={props.newOn ? "white": "black"}
                       d="M6.833 19.708 5.062 16.75l-3.374-.771.333-3.417L-.25 10l2.271-2.562-.333-3.417 3.374-.771L6.833.292 10 1.646 13.167.292l1.771 2.958 3.374.771-.333 3.417L20.25 10l-2.271 2.562.333 3.417-3.374.771-1.771 2.958L10 18.354Zm2.105-6.312 5.354-5.354-1.459-1.396-3.895 3.875-1.75-1.792-1.48 1.459Z"/>
             </svg>
